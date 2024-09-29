677b4997895808990e2c2eee869bc2c4c1bd3b9a
execebxo <maximmickevich@aol.com>
2024-09-29 13:57:14 +0200

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Max Mickevich on 29.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
